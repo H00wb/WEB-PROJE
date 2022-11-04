007d4a0e31f54982d84733075b20cd3d77905eb8
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2022-11-04 18:20:51 +0300

    CONTACT US TAMAMLANDI

---
+++ javascriptfile1.js
@@ -0,0 +1,15 @@
+function submitter()
+{
+var name = document.getElementById('name').value;
+var surname = document.getElementById('surname').value;
+var email = document.getElementById('email').value;
+var phone = document.getElementById('phone').value;
+if (name == '' && surname == '' && email == '' && phone == '')
+{
+alert("Please fill the blank fields");
+}
+else
+{
+alert("Name: " + name + " Surname: " + surname + "\n" +" Email: " + email + " Phone: " + phone);
+}
+}
