907a63f9bfa89bf3ad2fc57ca5a98e466a8ad2c0
Fidan Akdeniz <fidann.akdenzz@gmail.com>
2022-11-07 21:23:32 +0300

    x

--- javascriptfile1.js
+++ javascriptfile1.js
@@ -4,12 +4,14 @@ var name = document.getElementById('name').value;
 var surname = document.getElementById('surname').value;
 var email = document.getElementById('email').value;
 var phone = document.getElementById('phone').value;
-if (name == '' && surname == '' && email == '' && phone == '')
+
+if (name == '' || surname == '' || email == '' || phone =='')
 {
-alert("Please fill the blank fields");
+alert("Please fill the blank fields!");
 }
 else
 {
-alert("Name: " + name + " Surname: " + surname + "\n" +" Email: " + email + " Phone: " + phone);
+
+alert("Name: " + name + " Surname: " + surname + "\n" + "Phone Number: " + phone + " Email: " + email);
 }
 }
