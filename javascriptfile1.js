110cc28035d295cb897e706c0717df13c3c6e525
Emrehan Gökçay <101899529+H00wb@users.noreply.github.com>
2022-11-08 12:05:42 +0300

    xd

--- javascriptfile1.js
+++ javascriptfile1.js
@@ -4,14 +4,25 @@ var name = document.getElementById('name').value;
 var surname = document.getElementById('surname').value;
 var email = document.getElementById('email').value;
 var phone = document.getElementById('phone').value;
+var genderm= document.getElementById('genderm');
+var genderf= document.getElementById('genderf');
+var gender= '';
+if(genderf.checked)
+{
+    gender="Female";
+}
+else if(genderm.checked)
+{
+    gender="Male";
+}
 
-if (name == '' || surname == '' || email == '' || phone =='')
+if (name == '' || surname == '' || email == '' || phone =='' || gender=='')
 {
 alert("Please fill the blank fields!");
 }
 else
 {
 
-alert("Name: " + name + " Surname: " + surname + "\n" + "Phone Number: " + phone + " Email: " + email);
+alert("Name: " + name + " Surname: " + surname + "\n" + "Phone Number: " + phone + " Email: " + email + "\n" + "Gender: " + gender);
 }
 }
